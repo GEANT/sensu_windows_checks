29649565187ed19bc77374b2a5dce0495160dfce
Massimiliano Adamo <maxadamo@gmail.com>
2023-08-17 22:24:58 +0200

    fix again sha512sum

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ ASSET_NAME=$(basename $(pwd))
 ASSET_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1) | tr -d v)
 git-changelog > CHANGELOG.md
 tar czf "assets/${ASSET_NAME}-${ASSET_VERSION}.tar.gz" LICENSE README.md CHANGELOG.md bin/
-sha512sum assets/*.tar.gz > "assets/${ASSET_NAME}-${ASSET_VERSION}_sha512-checksums.txt"
+sha512sum assets/*.tar.gz | sed -e 's,assets/,,' > "assets/${ASSET_NAME}_sha512-checksums.txt"
 
