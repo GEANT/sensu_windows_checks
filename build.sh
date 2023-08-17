ad8f7c99a6b84f743fc93d2bf1cc681793e34d05
Massimiliano Adamo <maxadamo@gmail.com>
2023-08-17 21:47:46 +0200

    fix missing quotes in script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 ASSET_NAME=$(basename $(pwd))
 ASSET_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1) | tr -d v)
 git-changelog > CHANGELOG.md
-tar czf "${ASSET_NAME}-${ASSET_VERSION}.tar.gz LICENSE README.md CHANGELOG.md bin/
+tar czf "${ASSET_NAME}-${ASSET_VERSION}.tar.gz LICENSE README.md CHANGELOG.md bin/"
 
 sha512sum ./*.tar.gz > "${ASSET_NAME}-${ASSET_VERSION}_sha512-checksums.txt"
 
