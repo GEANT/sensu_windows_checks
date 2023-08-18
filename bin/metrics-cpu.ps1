2bfd9bc59171f9c914c4e1727fbdbdd90ecc37ba
Massimiliano Adamo <maxadamo@gmail.com>
2023-08-18 19:22:52 +0200

    minor

--- bin/metrics-cpu.ps1
+++ bin/metrics-cpu.ps1
@@ -29,8 +29,6 @@ param(
 $ThisProcess = Get-Process -Id $pid
 $ThisProcess.PriorityClass = "BelowNormal"
 
-. (Join-Path $PSScriptRoot perfhelper.ps1)
-
 if ($null -eq $EntityName) {
     #$EntityName = [System.Net.Dns]::GetHostByName($env:computerName).HostName
     #$EntityName = $EntityName.Replace(".", "_").ToLower()
@@ -38,26 +36,7 @@ if ($null -eq $EntityName) {
     $EntityName = "${env:computername}_${domainName}".ToLower()
 }
 
-$perfCategoryID = Get-PerformanceCounterByID -Name 'Processor Information'
-$localizedCategoryName = Get-PerformanceCounterLocalName -ID $perfCategoryID
+$cpu_pcnt_usage = (Get-CimInstance win32_processor | Measure-Object -Property LoadPercentage -Average).Average
+$Time = DateTimeToUnixTimestamp -DateTime (Get-Date)
 
-$counters = New-Object System.Collections.ArrayList
-
-[void]$counters.Add('% Processor Time')
-[void]$counters.Add('% Idle Time')
-[void]$counters.Add('% User Time')
-[void]$counters.Add('% Interrupt Time')
-
-foreach ($counter in $counters) {
-    $perfCounterID = Get-PerformanceCounterByID -Name $counter
-    $localizedCounterName = Get-PerformanceCounterLocalName -ID $perfCounterID
-    $value = [System.Math]::Round((Get-Counter "\$localizedCategoryName(_total)\$localizedCounterName" -SampleInterval 1 -MaxSamples 1).CounterSamples.CookedValue)
-
-    $Time = DateTimeToUnixTimestamp -DateTime (Get-Date)
-
-    if ($counter -eq '% Processor Time') { Write-Host "$EntityName.system $value $Time" }
-    if ($counter -eq '% Processor Time') { Write-Host "$EntityName.cpu.percent.total $value $Time" }
-    if ($counter -eq '% Idle Time') { Write-Host "$EntityName.cpu.percent.idle $value $Time" }
-    if ($counter -eq '% User Time') { Write-Host "$EntityName.cpu.percent.user $value $Time" }
-    if ($counter -eq '% Interrupt Time') { Write-Host "$EntityName.cpu.percent.interrupt $value $Time" }
-}
+Write-Host "$EntityName.system $cpu_pcnt_usage $Time"
